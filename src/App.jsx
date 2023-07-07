8df5770895cc233543afd60760e9ca4b821f190e
behzodmuxiddinov <behzodmufc@gmail.com>
2023-07-07 19:29:22 +0500

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,18 @@ import './App.css';
 import Filterbox from './components/filterbox/filterbox';
 function App(){
     const [inputValue, setInputValue] = useState("")
-    // const [todos, setTodos] = useState([])
     const [status, setStatus] = useState("")
-    const [filterTodo, setFilterTodo] = useState([])
-    // useEffect(()=>{
-    //     filterHandler()
-    // },[status,todos])
-
-    // const filterHandler = () =>{
-    //     switch(status){
-    //         case "completed" :
-    //             setFilterTodo(todos.filter((item) => item.completed === true))
-    //             break
-    //         case "uncompleted" :
-    //             setFilterTodo(todos.filter((item) => item.completed === false))
-    //             break
-    //         default :
-    //             setFilterTodo(todos)
-    //             break
-    //     }
-    // }
     return(
         <div className='app'>
             <Form
             inputValue={inputValue}
             setInputValue={setInputValue}
-            // todos={todos}
-            // setTodos={setTodos}
             />
             <TodoList 
-            // todos={todos}
-            // filterTodo={filterTodo}
             inputValue={inputValue}
-            // setTodos={setTodos}
             />
             <Filterbox 
-            // todos={todos}
             setStatus={setStatus}
-            // setTodos={setTodos}
             />
         </div>
     )
